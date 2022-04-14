ce23f32c3c81d4302bbc69ce1ea5a6d1b9cef94b
AyBalatsan <aybalatsan@yandex.ru>
2022-04-14 15:14:02 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(45deg, rgba(132,133,240,1) 0%, rgba(254,168,215,1) 100%);
+  width: 100%;
+  min-height: 100vh;
+  padding: 60px 40px;
+  position: relative;
+`
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <h1>settings default</h1>
+    </AppWrapper>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,72 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import styled, {createGlobalStyle, ThemeProvider} from 'styled-components' ;
+
+const Global = createGlobalStyle`
+*,*::before,*::after {
+  box-sizing: border-box;
+  outline: none;
+}
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  min-width: 320px;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+a {
+  text-decoration: none;
+}
+button {
+  cursor: pointer;
+  padding: 0;
+  background: transparent;
+  border: none;
+}
+
+button, input {
+  font-family: inherit;
+}
+
+p,h1,h2,h3,h4,h5 {
+  margin: 0;
+}
+
+ul,ol {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+`
+
+const theme = {
+  colors: {
+    primary: 'black',
+    secondary: 'red'
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global />
+      <App />   
+    </ThemeProvider>
   </React.StrictMode>
 );
 
